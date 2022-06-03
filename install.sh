b725f9e111400f2ec53b8f98e0685e155ceb5d6b
kup1o <nikitaack@yandex.ru>
2022-06-03 19:34:13 +0900

    add signing respository command
    gpg method is different from apt-key referred in [Spotify for Linux guide](https://spotify.com/download/linux)
    you can learn more about gpg at [Linux Uprising](https://www.linuxuprising.com/2021/01/apt-key-is-deprecated-how-to-add.html)

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo curl -fsSLo /usr/share/keyrings/spotify.gpg https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmour -o /usr/share/keyrings/spotify.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/spotify.gpg arch=amd64] http://repository.spotify.com stable non-free"|sudo tee /etc/apt/sources.list.d/spotify.list
+echo "deb [signed-by=/usr/share/keyrings/spotify.gpg arch=amd64] http://repository.spotify.com stable non-free"| sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt update
 
