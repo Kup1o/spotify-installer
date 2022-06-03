083be2da64e54d04f452e3ba512dbbcb2b0ccd3e
kup1o <nikitaack@yandex.ru>
2022-06-03 18:48:37 +0900

    update if statemenet

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ read method
 
 case $method in 
   "1")
-    if [$method -eq 1]; then
+    if ["$method" == 1]; then
       echo -e "You have chosen snap method to install Spotify Client"
       sudo snap install spotify
       echo -e "Spotify was succesfully installed!"
@@ -18,11 +18,11 @@ case $method in
       echo -en "Please specify 1 or 2 for choosing a method to install Spotify Client\n"
       echo -en "If you would like to install Spotify via Snap, write 1\n"
       echo -en "If you would like to install Spotify via Apt, write 2\n"
-      echo -en "Now run install.sh again!"
+      echo -en "Now run install.sh again!\n"
     exit
     fi;;
   "2")
-    if [$method -eq 2]; then
+    if ["$method" == 2]; then
       echo -e "You have chosen repository method to install Spotify Client\n"
       sudo curl -fsSLo /usr/share/keyrings/spotify.gpg https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg
       echo "deb [signed-by=/usr/share/keyrings/spotify.gpg arch=amd64] http://repository.spotify.com stable non-free"|sudo tee /etc/apt/sources.list.d/spotify.list
@@ -35,7 +35,7 @@ case $method in
       echo -en "Please specify 1 or 2 for choosing a method to install Spotify Client\n"
       echo -en "If you would like to install Spotify via Snap, write 1\n"
       echo -en "If you would like to install Spotify via Apt, write 2\n"
-      echo -en "Now run install.sh again!"
+      echo -en "Now run install.sh again!\n"
     exit
     fi
     break;;
